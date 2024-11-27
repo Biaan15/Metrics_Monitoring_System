cc3e9caf5322d7337ae303e254f99da1ad7eeea1
Biaan15 <biancafraga1599@gmail.com>
2024-11-27 00:36:16 -0300

    eliminar

--- build/build/Release/generators/conanrun.sh
+++
@@ -1,1 +0,0 @@
-. "/home/jorge/Desktop/sistemas-operativos-1/so-i-24-chp2-Jorgear27/Metrics_Monitoring_System/build/build/Release/generators/conanrunenv-release-x86_64.sh"
--- build/build/Release/generators/deactivate_conanbuild.sh
+++
@@ -1,1 +0,0 @@
-. "/home/jorge/Desktop/sistemas-operativos-1/so-i-24-chp2-Jorgear27/Metrics_Monitoring_System/build/build/Release/generators/deactivate_conanbuildenv-release-x86_64.sh"
